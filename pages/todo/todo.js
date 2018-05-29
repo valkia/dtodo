096d107448130b782a637d24ac56b05a915d9f63
610153956@qq.com <610153956@qq.com>
2018-05-30 00:41:09 +0800

    first commit

---
+++ pages/todo/todo.js
@@ -0,0 +1,47 @@
+//todo.js
+//路径根据自己项目路径修改
+var network = require("../../utils/network.js")
+const app = getApp()
+Page({
+  data: {
+    params: {
+      password
+      :
+      "123123",
+      username
+      :
+      "180000"
+    }
+
+  },
+  getList: function () {
+    network.requestLoading(app.url + "getList", this.data.params, '正在加载数据', function (res) {
+      //res就是我们请求接口返回的数据
+      console.log(res)
+
+    }, function () {
+      wx.showToast({
+        title: '加载数据失败',
+      })
+    })
+  },
+  login: function () {
+    network.requestLoading(app.url + "login", this.data.params, '正在加载数据', function (res) {
+      //res就是我们请求接口返回的数据
+      console.log(res)
+      //getList()
+    }, function () {
+      wx.showToast({
+        title: '加载数据失败',
+      })
+    })
+  },
+  
+  onLoad: function () {
+    this.login()
+  }
+})
+// 作者：Code4Android
+// 链接：https://www.jianshu.com/p/9796743d688f
+// 來源：简书
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
---
+++ utils/network.js
@@ -0,0 +1,59 @@
+function request(url, params, success, fail) {
+  this.requestLoading(url, params, "", success, fail)
+}
+// 展示进度条的网络请求
+// url:网络请求的url
+// params:请求参数
+// message:进度条的提示信息
+// success:成功的回调函数
+// fail：失败的回调
+function requestLoading(url, params, message, success, fail) {
+  console.log(params)
+  wx.showNavigationBarLoading()
+  if (message != "") {
+    wx.showLoading({
+      title: message,
+    })
+  }
+  wx.request({
+    url: url,
+    data: params,
+    header: {
+      'Content-Type': 'application/json'
+      //'content-type': 'application/x-www-form-urlencoded'
+    },
+    method: 'post',
+    success: function (res) {
+      //console.log(res.data)
+      wx.hideNavigationBarLoading()
+      if (message != "") {
+        wx.hideLoading()
+      }
+      if (res.statusCode == 200) {
+        success(res.data)
+      } else {
+        fail()
+      }
+
+    },
+    fail: function (res) {
+      wx.hideNavigationBarLoading()
+      if (message != "") {
+        wx.hideLoading()
+      }
+      fail()
+    },
+    complete: function (res) {
+
+    },
+  })
+}
+module.exports = {
+  request: request,
+  requestLoading: requestLoading
+}
+
+// 作者：Code4Android
+// 链接：https://www.jianshu.com/p/9796743d688f
+// 來源：简书
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
