a2278886c5e80fd2a552449e0c10e2684890f14e
dio <610153956@qq.com>
2018-06-07 18:12:20 +0800

    weui,todolist`s Tab

--- pages/todo/todo.js
+++ pages/todo/todo.js
@@ -1,47 +1,79 @@
-//todo.js
-//路径根据自己项目路径修改
-var network = require("../../utils/network.js")
-const app = getApp()
+// //todo.js
+// //路径根据自己项目路径修改
+// var network = require("../../utils/network.js")
+// const app = getApp()
+// Page({
+//   data: {
+//     params: {
+//       password
+//       :
+//       "123123",
+//       username
+//       :
+//       "180000"
+//     },
+//     list:[]
+
+//   },
+//   getList: function () {
+//     var that = this
+//     network.requestLoading(app.url + "task/getList", this.data.params, '正在加载数据', function (res) {
+//       //res就是我们请求接口返回的数据
+//       that.setData({
+//         list:res.data
+//       })
+
+//     }, function () {
+//       wx.showToast({
+//         title: '加载数据失败',
+//       })
+//     })
+//   },
+//   login: function () {
+//     network.requestLoading(app.url + "login", this.data.params, '正在加载数据', function (res) {
+//       //res就是我们请求接口返回的数据
+//       console.log(res)
+//       //getList()
+//     }, function () {
+//       wx.showToast({
+//         title: '加载数据失败',
+//       })
+//     })
+//   },
+  
+//   onLoad: function () {
+//     this.getList()
+//   }
+// })
+// // 作者：Code4Android
+// // 链接：https://www.jianshu.com/p/9796743d688f
+// // 來源：简书
+// // 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
 Page({
   data: {
-    params: {
-      password
-      :
-      "123123",
-      username
-      :
-      "180000"
-    }
-
+    tabs: ["选项一", "选项二"],
+    activeIndex: 1,
+    sliderOffset: 0,
+    sliderLeft: 0
   },
-  getList: function () {
-    network.requestLoading(app.url + "task/getList", this.data.params, '正在加载数据', function (res) {
-      //res就是我们请求接口返回的数据
-      console.log(res)
-
-    }, function () {
-      wx.showToast({
-        title: '加载数据失败',
-      })
-    })
-  },
-  login: function () {
-    network.requestLoading(app.url + "login", this.data.params, '正在加载数据', function (res) {
-      //res就是我们请求接口返回的数据
-      console.log(res)
-      //getList()
-    }, function () {
-      wx.showToast({
-        title: '加载数据失败',
-      })
-    })
-  },
-  
   onLoad: function () {
-    this.getList()
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
   }
-})
-// 作者：Code4Android
-// 链接：https://www.jianshu.com/p/9796743d688f
-// 來源：简书
-// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+});
